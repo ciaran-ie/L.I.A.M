42b52ffba4afaab6d900549cb001f9336bca7e2e
ciaran-ie <ciaranott@gmail.com>
2024-06-27 20:49:18 +0100

    Create __init__.py

---
+++ app/__init__.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object('config.Config')
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    with app.app_context():
+        from . import routes, models
+        db.create_all()
+
+    return app
+
+app = create_app()
