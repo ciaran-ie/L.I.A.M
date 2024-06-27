08f199e746d4ba814fdacf81019b3463710c3032
ciaran-ie <ciaranott@gmail.com>
2024-06-27 20:07:06 +0100

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'my_precious')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///site.db')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///liam.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
